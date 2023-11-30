44c4d76bd6c590faa0de2b72b9800c5f40763c96
ErichLeonardo <erich.hamm21@gmail.com>
2023-11-30 19:59:44 +0100

    noteavideo8

---
+++ src/enviroments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+        production: false,
+        firebaseConfig: {
+                apiKey: "AIzaSyAyJtUqXUO5chP0EZnYxKrNd9fZxeDCvm8",      
+                authDomain: "noteafinal.firebaseapp.com",      
+                projectId: "noteafinal",      
+                storageBucket: "noteafinal.appspot.com",      
+                messagingSenderId: "49131526110",     
+                appId: "1:49131526110:web:1ba732a27e00a6d11a2f4f",
+                measurementId: "G-14PZKRQQNQ"
+            }
+};
